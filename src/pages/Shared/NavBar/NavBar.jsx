94582fc49f15eaef1ab80cf5cbf23084471dab79
Mohammad Sakib <mohammadsakib7679@gmail.com>
2023-07-18 18:46:01 +0600

    banner added

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
     </>
   );
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar fixed z-10 bg-opacity-10 bg-black text-white font-bold max-w-6xl">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
